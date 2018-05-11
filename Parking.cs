1f8b675b05b0f17a23018394c43be63a79578130
Abiev Andrew <andreyka.abiev@gmail.com>
2018-05-11 08:26:06 +0300

    Create singlet with settings :)

---
+++ Parking.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParkingSpace
+{
+    class Parking
+    {
+        private static readonly Lazy<Parking> lazy = new Lazy<Parking>(() => new Parking());
+
+        public static int currentId = 1;
+        private double Profit { get; set; }
+        private List<Car> area;
+        private List<Transaction> transaction;
+
+        private uint timeout;
+        private int parkingSpace;
+        private double fine;
+
+        protected Parking()
+        {
+            area = new List<Car>();
+            transaction = new List<Transaction>();
+
+            timeout = Settings.Timeout;
+            parkingSpace = Settings.ParkingSpace;
+            fine = Settings.Fine;
+        }
+
+        public static Parking Create
+        {
+            get
+            {
+                return lazy.Value;
+            }
+        }
+    }
+}
